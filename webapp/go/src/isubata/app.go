dae618e8e911cd66c81595f8f206500c489b8afb
masimacbook <masi19bw@gmail.com>
2020-06-04 05:53:56 +0000

    decrease n+1

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -122,12 +122,12 @@ type Message struct {
 	CreatedAt time.Time `db:"created_at"`
 }
 
-func queryMessages(chanID, lastID int64) ([]Message, error) {
-	msgs := []Message{}
-	err := db.Select(&msgs, "SELECT * FROM message WHERE id > ? AND channel_id = ? ORDER BY id DESC LIMIT 100",
-		lastID, chanID)
-	return msgs, err
-}
+// func queryMessages(chanID, lastID int64) ([]Message, error) {
+// 	msgs := []Message{}
+// 	err := db.Select(&msgs, "SELECT * FROM message WHERE id > ? AND channel_id = ? ORDER BY id DESC LIMIT 100",
+// 		lastID, chanID)
+// 	return msgs, err
+// }
 
 func sessUserID(c echo.Context) int64 {
 	sess, _ := session.Get("session", c)
@@ -381,7 +381,19 @@ func getMessage(c echo.Context) error {
 		return err
 	}
 
-	messages, err := queryMessages(chanID, lastID)
+
+type MessageWithUser struct {
+		ID              int64     `db:"id"`
+		Content         string    `db:"content"`
+		CreatedAt       time.Time `db:"created_at"`
+		UserName        string    `db:"name"`
+		UserDisplayName string    `db:"display_name"`
+		UserAvatarIcon  string    `db:"avatar_icon"`
+	}
+	messages := []MessageWithUser{}
+	err = db.Select(&messages, "select m.id, m.content, m.created_at, u.name, u.display_name, u.avatar_icon from message m inner join user u on u.id = m.user_id where m.id > ? and m.channel_id = ? order by m.id desc limit 100", lastID, chanID)
+
+	//messages, err := queryMessages(chanID, lastID)
 	if err != nil {
 		return err
 	}
@@ -389,10 +401,15 @@ func getMessage(c echo.Context) error {
 	response := make([]map[string]interface{}, 0)
 	for i := len(messages) - 1; i >= 0; i-- {
 		m := messages[i]
-		r, err := jsonifyMessage(m)
-		if err != nil {
-			return err
-		}
+	r := make(map[string]interface{})
+		if err != nil {			r["id"] = m.ID
+			return err			r["user"] = User{
+			Name:        m.UserName,
+			DisplayName: m.UserDisplayName,
+			AvatarIcon:  m.UserAvatarIcon,
+		}			}
+		r["date"] = m.CreatedAt.Format("2006/01/02 15:04:05")
+		r["content"] = m.Content
 		response = append(response, r)
 	}
 
